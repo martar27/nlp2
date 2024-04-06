252e88b9f0bccdc93d30281076679e8246619580
martar27 <125350158+martar27@users.noreply.github.com>
2024-04-07 01:33:46 +0300

    Update train.py with training functionality from NLP_2_2.ipynb

--- train.py
+++ train.py
@@ -1,9 +1,27 @@
 import torch
 from torch.utils.data import DataLoader
+from model import PGNet, Encoder, Decoder, Attention
+from data_loader import load_and_preprocess_data
+import torch.optim as optim
+import torch.nn as nn
 
-def train_model():
-    # Initialize model, loss function, optimizer...
-    # Train the model...
+def train_model(dataset, model, optimizer, criterion, num_epochs=10):
+    for epoch in range(num_epochs):
+        model.train()
+        total_loss = 0
+        for dialogues, summaries in DataLoader(dataset['train'], batch_size=32, shuffle=True):
+            optimizer.zero_grad()
+            output = model(dialogues, summaries)
+            loss = criterion(output, summaries)
+            loss.backward()
+            optimizer.step()
+            total_loss += loss.item()
+        print(f'Epoch {epoch+1}, Loss: {total_loss}')
 
-if __name__ == "__main__":
-    train_model()
+if __name__ == '__main__':
+    datasets = load_and_preprocess_data()
+    model = PGNet()
+    optimizer = optim.Adam(model.parameters(), lr=0.001)
+    criterion = nn.CrossEntropyLoss()
+    train_model(datasets, model, optimizer, criterion)
+
