0b416522be85153b0f5c4cb4137ecfd39e7141e2
martar27 <125350158+martar27@users.noreply.github.com>
2024-04-07 01:18:25 +0300

    Add train.py script

---
+++ train.py
@@ -0,0 +1,9 @@
+import torch
+from torch.utils.data import DataLoader
+
+def train_model():
+    # Initialize model, loss function, optimizer...
+    # Train the model...
+
+if __name__ == "__main__":
+    train_model()
