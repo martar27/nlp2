388c387ef7e188c5b972b11f7d057b0118c1a90b
martar27 <125350158+martar27@users.noreply.github.com>
2024-04-07 12:47:34 +0300

    Update train.py

--- train.py
+++ train.py
@@ -6,14 +6,16 @@ import torch.optim as optim
 import torch.nn as nn
 import os
 
-def train_model(dataset, model, optimizer, criterion, num_epochs=3):
+num_epochs = 3
+
+def train_model(dataset, model, optimizer, criterion, num_epochs=num_epochs):
     best_val_loss = float('inf')
     for epoch in range(num_epochs):
         model.train()
         total_loss = 0
-        for dialogues, summaries in DataLoader(dataset['train'], batch_size=8, shuffle=True):
+        for dialogues, summaries, input_lengths in DataLoader(dataset['train'], batch_size=8, shuffle=True):
             optimizer.zero_grad()
-            output = model(dialogues, summaries)
+            output = model(dialogues, input_lengths)
             loss = criterion(output, summaries)
             loss.backward()
             optimizer.step()
@@ -24,8 +26,8 @@ def train_model(dataset, model, optimizer, criterion, num_epochs=3):
         model.eval()
         total_val_loss = 0
         with torch.no_grad():
-            for dialogues, summaries in DataLoader(dataset['validation'], batch_size=8, shuffle=False):
-                output = model(dialogues, summaries)
+            for dialogues, summaries, input_lengths in DataLoader(dataset['validation'], batch_size=8, shuffle=False):
+                output = model(dialogues, input_lengths)
                 val_loss = criterion(output, summaries)
                 total_val_loss += val_loss.item()
         print(f'Validation Loss: {total_val_loss}')
@@ -54,9 +56,9 @@ if __name__ == '__main__':
     for epoch in range(num_epochs):
         model.train()
         total_loss = 0
-        for dialogues, summaries in train_loader:
+        for dialogues, summaries, input_lengths in train_loader:
             optimizer.zero_grad()
-            output = model(dialogues, summaries)
+            output = model(dialogues, input_lengths)
             loss = criterion(output, summaries)
             loss.backward()
             optimizer.step()
@@ -69,8 +71,8 @@ model.load_state_dict(torch.load('best_model.pth'))
 model.eval()
 total_test_loss = 0
 with torch.no_grad():
-    for dialogues, summaries in DataLoader(datasets['test'], batch_size=32, shuffle=False):
-        output = model(dialogues, summaries)
+    for dialogues, summaries, input_lengths in DataLoader(datasets['test'], batch_size=32, shuffle=False):
+        output = model(dialogues, input_lengths)
         test_loss = criterion(output, summaries)
         total_test_loss += test_loss.item()
 print(f'Test Loss: {total_test_loss}')
@@ -78,3 +80,4 @@ model = PGNet()
 optimizer = optim.Adam(model.parameters(), lr=0.001)
 criterion = nn.CrossEntropyLoss()
 train_model(datasets, model, optimizer, criterion)
+
