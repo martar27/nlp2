a74a3345e11874934aa7f59d263cf06cf3126e77
martar27 <125350158+martar27@users.noreply.github.com>
2024-04-07 11:14:25 +0300

    Update data_loader.py with collate_fn for proper batching

--- data_loader.py
+++ data_loader.py
@@ -1,6 +1,7 @@
 from datasets import load_dataset
 from torch.utils.data import Dataset, DataLoader
 from nltk.tokenize import word_tokenize
+from torch.nn.utils.rnn import pad_sequence
 import torch
 import nltk
 
@@ -19,6 +20,12 @@ class DialogueSummaryDataset(Dataset):
         dialogue = [self.vocab.get(word, self.vocab['<unk>']) for word in word_tokenize(self.dialogues[idx].lower())]
         summary = [self.vocab.get(word, self.vocab['<unk>']) for word in word_tokenize(self.summaries[idx].lower())]
         return torch.tensor(dialogue, dtype=torch.long), torch.tensor(summary, dtype=torch.long)
+
+def collate_fn(batch):
+    dialogues, summaries = zip(*batch)
+    dialogues_padded = pad_sequence(dialogues, batch_first=True, padding_value=0)
+    summaries_padded = pad_sequence(summaries, batch_first=True, padding_value=0)
+    return dialogues_padded, summaries_padded
 
 def build_vocab(dataset):
     vocab = {'<pad>': 0, '<unk>': 1, '<sos>': 2, '<eos>': 3}
@@ -45,28 +52,10 @@ def load_and_preprocess_data():
 if __name__ == "__main__":
     datasets = load_and_preprocess_data()
     for split, dataset in datasets.items():
-        loader = DataLoader(dataset, batch_size=32, shuffle=True)
-        for batch in loader:
-            print(batch)
-    dataset = load_dataset("samsum")
-    vocab = build_vocab(dataset)
-    processed_datasets = {}
-    for split in ['train', 'test', 'validation']:
-        dialogues = [item['dialogue'] for item in dataset[split]]
-        summaries = [item['summary'] for item in dataset[split]]
-        processed_datasets[split] = DialogueSummaryDataset(dialogues, summaries, vocab)
-    return processed_datasets
-
-if __name__ == "__main__":
-    datasets = load_and_preprocess_data()
-    for split, dataset in datasets.items():
-        loader = DataLoader(dataset, batch_size=32, shuffle=True)
+        loader = DataLoader(dataset, batch_size=32, shuffle=True, collate_fn=collate_fn)
         print(f'Processing {split} data:')
-        for batch in loader:
-            print(batch)
-
-if __name__ == "__main__":
-    dataset = load_and_preprocess_data()
+        for dialogues, summaries in loader:
+            print(dialogues.shape, summaries.shape)
     loader = DataLoader(dataset, batch_size=32, shuffle=True)
     for batch in loader:
         print(batch)
